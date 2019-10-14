4c28f027586fc0008c7f9bb9a37d172be9338589
progwebe <progwebe@gmail.com>
2019-10-14 11:19:23 +0000

    build

---
+++ build/precache-manifest.00c0f68d83d5bf9c1df6b9219ffc528c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1ae6d199b60403f6425bf83d8a0d75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec9ffdefef186622e288",
+    "url": "/static/css/main.df713943.chunk.css"
+  },
+  {
+    "revision": "0f91c43b7f6baa5d1551",
+    "url": "/static/js/2.4c76e457.chunk.js"
+  },
+  {
+    "revision": "ec9ffdefef186622e288",
+    "url": "/static/js/main.132770b3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
